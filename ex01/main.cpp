e52b1f1acd8616149f3fa6841d21c65f73131353
yu-432 <o.shimayu891@gmail.com>
2025-02-04 16:08:07 +0900

    :art: typo

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,38 +6,35 @@
 /*   By: yooshima <yooshima@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 09:22:31 by yooshima          #+#    #+#             */
-/*   Updated: 2025/02/04 09:41:33 by yooshima         ###   ########.fr       */
+/*   Updated: 2025/02/04 16:07:46 by yooshima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
 #include "iter.hpp"
 
-void incriment(int& i) {
-  i++;
-}
+void increment(int& i) { i++; }
 
-void addHello(std::string& str) {
-  str = str + "Hello";
-}
+void addHello(std::string& str) { str = str + "Hello"; }
 
 int main(void) {
   int array[10] = {0};
-  std::string strs[5] = {"poti", "tama", "udon", "nikku", "cacao"}; 
-  
+  std::string strs[5] = {"poti", "tama", "udon", "nikku", "cacao"};
+
   for (int i = 0; i < 10; ++i) std::cout << array[i];
   std::cout << std::endl;
-  
-  ::iter(array, 10, &incriment);
-  
+
+  ::iter(array, 10, &increment);
+
   for (int i = 0; i < 10; ++i) std::cout << array[i];
   std::cout << std::endl;
 
   for (int i = 0; i < 5; ++i) std::cout << strs[i] << ", ";
   std::cout << std::endl;
-  
+
   ::iter(strs, 5, &addHello);
-  
+
   for (int i = 0; i < 5; ++i) std::cout << strs[i] << ", ";
   std::cout << std::endl;
 }
